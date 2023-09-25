051f3272ba5d3489a91f0a38a1183bc84534ccbd
Jean <jeanttalar@gmail.com>
2023-09-25 10:35:10 -0300

    feat: finished layout and functionality

--- day-19/challenge/script.js
+++ day-19/challenge/script.js
@@ -5,4 +5,6 @@ navToggle.addEventListener('click', toggleMenu);
 function toggleMenu(event) {
   event.preventDefault();
   navToggle.classList.toggle('active');
+  const nav = document.querySelector('.nav');
+  nav.classList.toggle('active');
 }
